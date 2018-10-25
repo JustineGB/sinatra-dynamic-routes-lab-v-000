7d5a5c503cdb760011077903497ef8ed3c5ee319
Justine B <justinegayton@gmail.com>
2018-10-25 14:16:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,14 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/reversename/:name' do
-    params[:name].reverse
+    @name = params[:name].reverse
+    "#{@name}"
   end
 
   get '/square/:number' do
+    @number = params[:phrase]
   end
 
   get '/say/:number/:phrase' do
+    @phrase = params[:phrase]
+
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
@@ -16,7 +21,7 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     @answer = params[:number1] + params[:number2]
-    @answer 
+    @answer
   end
 
 
