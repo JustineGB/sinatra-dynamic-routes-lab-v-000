53b3da5863497bd00bdced29c778e6b6096dea7e
Justine B <justinegayton@gmail.com>
2018-10-25 14:19:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,8 +22,9 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @answer = params[:number1] + params[:number2]
-    @answer
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
   end
 
 
